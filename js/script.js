465cbbb6e94ac5b58dcaf93fd6ffd4884eff3fdc
simolorevg <simoneviaggi@libero.it>
2023-03-22 14:04:29 +0100

    Working on the script

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const teamPeople=[
+    {
+        name: 'Wayne Barnett',
+        task: 'Founder & CEO',
+        imgSelf:'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        task: 'Chief Editor',
+        imgSelf:'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon' ,
+        task: 'Office Manager',
+        imgSelf: ''
+    },
+    {
+        name: '' ,
+        task: '',
+        imgSelf: ''
+    },
+    {
+        name: '' ,
+        task: '',
+        imgSelf: ''
+    },
+    {
+        name: '' ,
+        task: '',
+        imgSelf: ''
+    }
+]
